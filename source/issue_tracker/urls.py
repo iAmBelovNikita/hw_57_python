caa6a0c16671d910b7d0095a1285b78ca78d13f1
iAmBelovNikita <iambelovnikita@gmail.com>
2026-06-11 17:32:00 +0600

    Add logic for detail view.

--- source/issue_tracker/urls.py
+++ source/issue_tracker/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import TaskListView
+from .views import TaskListView, TaskDetailView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="list"),
+    path("task/<int:pk>/", TaskDetailView.as_view(), name="detail"),
 ]
--- source/issue_tracker/views.py
+++ source/issue_tracker/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from .models import Task
 
@@ -9,4 +10,12 @@ class TaskListView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = Task.objects.all().order_by('-created_at')
+        return context
+
+class TaskDetailView(TemplateView):
+    template_name = "detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Task, pk=self.kwargs.get('pk'))
         return context
