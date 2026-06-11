5bcdd4f2a47ebf08ed5f3db13d05445fafc536af
iAmBelovNikita <iambelovnikita@gmail.com>
2026-06-11 12:16:33 +0600

    Make migrate. Add all data and make fixture

--- source/issue_tracker/admin.py
+++ source/issue_tracker/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Task, Status, Type
 
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'summary', 'status', 'type', 'created_at', 'updated_at')
+    list_filter = ('status', 'type', 'created_at')
+    search_fields = ('summary', 'description')
+    readonly_fields = ('created_at', 'updated_at')
+
+    fields = (
+        'summary',
+        'description',
+        'status',
+        'type',
+        'created_at',
+        'updated_at',
+    )
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Type, TypeAdmin)
