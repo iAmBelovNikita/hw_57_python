ab2e35a06a031e704840bf41402322f3dc8c729c
iAmBelovNikita <iambelovnikita@gmail.com>
2026-06-15 16:53:14 +0600

    Add 2 custom validators

--- source/issue_tracker/forms.py
+++ source/issue_tracker/forms.py
@@ -1,20 +1,32 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import TextInput, Textarea
 from .models import Task, Status, Type
 
+def validate_summary_min_length(value):
+    if len(value.strip()) < 3:
+        raise ValidationError('Summary should be minimum 3 characters long')
+
+def validate_description_no_spam(value):
+    forbidden = ['fuck', 'shit', 'shitfuck']
+    for word in forbidden:
+        if word.lower() in value.lower():
+            raise ValidationError(f"Description shouldn't contain word: '{word}'")
 
 class TaskForm(forms.ModelForm):
     summary = forms.CharField(
         required=True,
         max_length=200,
         label="Summary",
-        widget=TextInput(attrs={"class": "form-control"})
+        widget=TextInput(attrs={"class": "form-control"}),
+        validators=[validate_summary_min_length],
     )
 
     description = forms.CharField(
         required=False,
         label="Description",
-        widget=Textarea(attrs={"class": "form-control", "rows": "5"})
+        widget=Textarea(attrs={"class": "form-control", "rows": "5"}),
+        validators=[validate_description_no_spam],
     )
 
     status = forms.ModelChoiceField(
