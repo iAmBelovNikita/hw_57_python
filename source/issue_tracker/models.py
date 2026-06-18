fb140a1a87faf313b06edce7c526b137fc7ebfb9
iAmBelovNikita <iambelovnikita@gmail.com>
2026-06-18 13:56:42 +0600

    Make migrate with new project not required and then make project required

--- source/issue_tracker/models.py
+++ source/issue_tracker/models.py
@@ -37,6 +37,7 @@ class Task(models.Model):
     description = models.TextField(blank=True, null=True)
     status = models.ForeignKey(Status, on_delete=models.RESTRICT)
     type = models.ManyToManyField(Type, related_name='tasks')
+    project = models.ForeignKey(Project, on_delete=models.RESTRICT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
