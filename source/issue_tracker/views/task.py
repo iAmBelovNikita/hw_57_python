d9c28fe6d4b26836cba733a0746bdcfb5effa70c
iAmBelovNikita <iambelovnikita@gmail.com>
2026-06-18 18:04:37 +0600

    Minor edits and refactor task.py for Django "*View's"

--- source/issue_tracker/views/task.py
+++ source/issue_tracker/views/task.py
@@ -1,28 +1,32 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, UpdateView, DeleteView
+from django.urls import reverse
 from ..models import Task, Project
 from ..forms import TaskForm
 
-# Create your views here.
 
-class TaskDetailView(TemplateView):
+class TaskDetailView(DetailView):
+    model = Task
     template_name = "task/detail.html"
+    context_object_name = "task"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['task'] = get_object_or_404(Task, pk=self.kwargs.get('pk'))
-        context['project'] = context['task'].project
+        context['project'] = self.object.project
         return context
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Task, pk=self.kwargs.get('pk'))
 
 class TaskCreateView(View):
     def get(self, request, pk):
-        project = get_object_or_404(Project, pk=self.kwargs.get('pk'))
+        project = get_object_or_404(Project, pk=pk)
         form = TaskForm()
         return render(request, "task/create.html", {"form": form, "project": project})
 
     def post(self, request, pk):
-        project = get_object_or_404(Project, pk=self.kwargs.get('pk'))
+        project = get_object_or_404(Project, pk=pk)
         form = TaskForm(request.POST)
         if form.is_valid():
             task = form.save(commit=False)
@@ -32,26 +36,34 @@ class TaskCreateView(View):
             return redirect("project-detail", pk=project.pk)
         return render(request, "task/create.html", {"form": form, "project": project})
 
-class TaskUpdateView(View):
-    def dispatch(self, request, *args, **kwargs):
-        self.task = get_object_or_404(Task, pk=self.kwargs.get('pk'))
-        return super().dispatch(request, *args, **kwargs)
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "task/update.html"
+    context_object_name = "task"
 
-    def get(self, request, pk):
-        form = TaskForm(instance=self.task)
-        return render(request, "task/update.html", {"form": form, "task": self.task})
+    def get_object(self, queryset=None):
+        return get_object_or_404(Task, pk=self.kwargs.get('pk'))
 
-    def post(self, request, pk):
-        form = TaskForm(request.POST, instance=self.task)
-        if form.is_valid():
-            task = form.save()
-            task.type.set(form.cleaned_data['type'])
-            return redirect("detail", pk=task.pk)
-        return render(request, "task/update.html", {"form": form, "task": self.task})
+    def form_valid(self, form):
+        task = form.save()
+        task.type.set(form.cleaned_data['type'])
+        return redirect('detail', pk=task.pk)
 
-class TaskDeleteView(View):
-    def post(self, request, *args, **kwargs):
-        task = get_object_or_404(Task, pk=self.kwargs.get('pk'))
-        project_pk = task.project.pk
-        task.delete()
-        return redirect("project-detail", pk=project_pk)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = self.object
+        return context
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Task, pk=self.kwargs.get('pk'))
+
+    def get_success_url(self):
+        return reverse('project-detail', kwargs={'pk': self.object.project.pk})
+
+    def get(self, request, *args, **kwargs):
+        return redirect('project-detail', pk=self.get_object().project.pk)
