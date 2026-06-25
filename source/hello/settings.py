9b88e7887055b4992fe49d24070b0d4f50fce65f
iAmBelovNikita <iambelovnikita@gmail.com>
2026-06-25 14:40:24 +0600

    Create new app 'accounts' and add in settings

--- source/hello/settings.py
+++ source/hello/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'issue_tracker'
+    'issue_tracker',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = 'project-list'
+LOGOUT_REDIRECT_URL = 'login'
